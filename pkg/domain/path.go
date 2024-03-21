f2df606e12e140b8247d7591566399fd74a71877
Felipe Dornelas <m@felipedornelas.com>
2024-03-20 23:18:51 -0300

    Path is a subset of Digraph
    ...where for each point, or node, there can only exist at most one
    inbound connection, and at most one outbound connection.
    
    All points in the path must be connected to form one path, i.e., there
    should exist no partitions. Also, there should exist no cycles or branches
    in the path.

---
+++ pkg/domain/path.go
@@ -0,0 +1,76 @@
+package domain
+
+import (
+	"errors"
+)
+
+// Path is a subset of Digraph, where for each point, or node, there can only exist at most one inbound connection, and
+// at most one outbound connection.
+//
+// All points in the path must be connected to form one path, i.e., there should exist no partitions. Also, there
+// should exist no cycles or branches in the path.
+type Path[T comparable] struct {
+	points     map[T]bool
+	outboundOf map[T]T
+	inboundOf  map[T]T
+}
+
+func NewPath[T comparable]() *Path[T] {
+	return &Path[T]{
+		points:     make(map[T]bool),
+		outboundOf: make(map[T]T),
+		inboundOf:  make(map[T]T),
+	}
+}
+
+func (p *Path[T]) AddConnection(from, to T) error {
+	if from == to {
+		return errors.New(`unable to add connection - "from" and "to" are the same`)
+	}
+
+	var nullValue T
+
+	if p.outboundOf[from] != nullValue {
+		return errors.New(`unable to add connection - "from" already has an outbound connection`)
+	}
+
+	if p.inboundOf[to] != nullValue {
+		return errors.New(`unable to add connection - "to" already has an inbound connection`)
+	}
+
+	p.points[from] = true
+	p.points[to] = true
+
+	p.outboundOf[from] = to
+	p.inboundOf[to] = from
+
+	return nil
+}
+
+func (p *Path[T]) FindStart() (T, error) {
+	var nullValue T
+
+	for i := range p.points {
+		if p.inboundOf[i] == nullValue {
+			return i, nil
+		}
+	}
+
+	return nullValue, errors.New("unable to find start of path - there's a loop")
+}
+
+func (p *Path[T]) FindEnd() (T, error) {
+	var nullValue T
+
+	for i := range p.points {
+		if p.outboundOf[i] == nullValue {
+			return i, nil
+		}
+	}
+
+	return nullValue, errors.New("unable to find end of path - there's a loop")
+}
+
+func (p *Path[T]) GetNext(a T) T {
+	return p.outboundOf[a]
+}
---
+++ pkg/domain/path_test.go
@@ -0,0 +1,121 @@
+package domain
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPath_AddConnection_FailsIfPointsAreTheSame(t *testing.T) {
+	p := NewPath[string]()
+	err := p.AddConnection("foo", "foo")
+	assert.EqualError(t, err, `unable to add connection - "from" and "to" are the same`)
+}
+
+func TestPath_AddConnection_FailsIfFromAlreadyHasAnOutboundConnection(t *testing.T) {
+	p := NewPath[string]()
+
+	err := p.AddConnection("foo", "bar")
+	assert.NoError(t, err)
+
+	err = p.AddConnection("foo", "baz")
+
+	assert.EqualError(t, err, `unable to add connection - "from" already has an outbound connection`)
+}
+
+func TestPath_AddConnection_FailsIfToAlreadyHasAnInboundConnection(t *testing.T) {
+	p := NewPath[string]()
+
+	err := p.AddConnection("foo", "bar")
+	assert.NoError(t, err)
+
+	err = p.AddConnection("baz", "bar")
+
+	assert.EqualError(t, err, `unable to add connection - "to" already has an inbound connection`)
+}
+
+func TestPath_AddConnection_FailsIfAddingTheSameConnectionTwice(t *testing.T) {
+	p := NewPath[string]()
+
+	err := p.AddConnection("foo", "bar")
+	assert.NoError(t, err)
+
+	err = p.AddConnection("foo", "bar")
+
+	assert.EqualError(t, err, `unable to add connection - "from" already has an outbound connection`)
+}
+
+func TestPath_FindStart(t *testing.T) {
+	p := NewPath[string]()
+
+	assert.NoError(t, p.AddConnection("c", "d"))
+	assert.NoError(t, p.AddConnection("a", "b"))
+	assert.NoError(t, p.AddConnection("b", "c"))
+
+	start, err := p.FindStart()
+	assert.NoError(t, err)
+	assert.Equal(t, start, "a")
+}
+
+func TestPath_FindStart_FailsIfTheresALoop(t *testing.T) {
+	p := NewPath[string]()
+
+	assert.NoError(t, p.AddConnection("a", "b"))
+	assert.NoError(t, p.AddConnection("b", "c"))
+	assert.NoError(t, p.AddConnection("c", "d"))
+	assert.NoError(t, p.AddConnection("d", "a"))
+
+	_, err := p.FindStart()
+	assert.EqualError(t, err, "unable to find start of path - there's a loop")
+}
+
+func TestPath_FindEnd(t *testing.T) {
+	p := NewPath[string]()
+
+	assert.NoError(t, p.AddConnection("b", "c"))
+	assert.NoError(t, p.AddConnection("d", "e"))
+	assert.NoError(t, p.AddConnection("a", "b"))
+	assert.NoError(t, p.AddConnection("c", "d"))
+
+	end, err := p.FindEnd()
+	assert.NoError(t, err)
+	assert.Equal(t, end, "e")
+}
+
+func TestPath_FindEnd_FailIfTheresALoop(t *testing.T) {
+	p := NewPath[string]()
+
+	assert.NoError(t, p.AddConnection("a", "b"))
+	assert.NoError(t, p.AddConnection("b", "c"))
+	assert.NoError(t, p.AddConnection("c", "d"))
+	assert.NoError(t, p.AddConnection("d", "e"))
+	assert.NoError(t, p.AddConnection("e", "a"))
+
+	_, err := p.FindEnd()
+	assert.EqualError(t, err, "unable to find end of path - there's a loop")
+}
+
+func TestPath_NavigateThePath(t *testing.T) {
+	p := NewPath[string]()
+
+	assert.NoError(t, p.AddConnection("d", "e"))
+	assert.NoError(t, p.AddConnection("b", "c"))
+	assert.NoError(t, p.AddConnection("c", "d"))
+	assert.NoError(t, p.AddConnection("e", "f"))
+	assert.NoError(t, p.AddConnection("a", "b"))
+
+	start, err := p.FindStart()
+	assert.NoError(t, err)
+	assert.Equal(t, start, "a")
+
+	assert.Equal(t, p.GetNext("a"), "b")
+	assert.Equal(t, p.GetNext("b"), "c")
+	assert.Equal(t, p.GetNext("c"), "d")
+	assert.Equal(t, p.GetNext("d"), "e")
+	assert.Equal(t, p.GetNext("e"), "f")
+	assert.Equal(t, p.GetNext("f"), "")
+
+	end, err := p.FindEnd()
+	assert.NoError(t, err)
+	assert.Equal(t, end, "f")
+}
