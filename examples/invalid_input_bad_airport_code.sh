ac7b6d507a75953cb22bcad27eea414255ef8f95
Felipe Dornelas <m@felipedornelas.com>
2024-03-25 00:36:20 -0300

    Invalid airport code example script

---
+++ examples/invalid_input_bad_airport_code.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl -0 -v http://localhost:8080/calculate \
+-H "Expect:" \
+-H 'Content-Type: application/json' \
+--data-binary @- << EOF
+{
+    "flight_legs": [
+        ["IND", "EWR"],
+        ["SFO", "HIGH5"],
+        ["GSO", "IND"],
+        ["ATL", "GSO"]
+    ]
+}
+EOF
